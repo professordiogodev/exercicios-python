84c988f184b353521d1d9ac36d3e672b4bc10d0f
professordiogodev <diogo@dionamite.com>
2026-01-05 15:42:35 +0000

    adicionar ficheiro inicial

---
+++ app.py
@@ -0,0 +1,32 @@
+# Criar stats para dois personagens
+# Fazer um personagem atacar outro (sem funções)
+
+# Variáveis para Diogo
+diogo_hp = 40
+diogo_attack = 5
+diogo_defense = 3
+
+# Variáveis para Raquel
+raquel_hp = 50
+raquel_attack = 3
+raquel_defense = 5
+
+# Resolução Ex 2:
+# Diogo Ataca Raquel
+print("Diogo ataca Raquel!")
+raquel_hp -= diogo_attack # Passas a ter 45 de hp
+
+# Raquel Ataca Diogo
+print("Raquel ataca Diogo!")
+diogo_hp -= raquel_attack # Passas a ter 45 de hp
+
+# Resolução Ex 1:
+
+
+
+
+
+
+# Ex 1 - Mostrar Resultados (Raquel ficou com x pontos de vida!)
+# Ex 2 - Colocar a defesa ao barulho:
+## Exemplo: Se diogo atacar Raquel, vai ser 50 - (5 - 5), vai dar 0 de dano.
