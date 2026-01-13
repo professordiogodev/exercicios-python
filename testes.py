a730ab6643dfe3bc378674563166a4dd1fbf9d16
Diogo Barros <119113180+professordiogodev@users.noreply.github.com>
2026-01-13 16:03:56 +0000

    Exercício de while + ficheiros (incompleto)

--- testes.py
+++ testes.py
@@ -1,12 +1,61 @@
-raquel_hp = 60
-raquel_attack = 4
-current_monster = "Slime"
+raquel = {
+    "hp": 40,
+    "attack": 6,
+    "current_monster": "Goblin"
+}
 
-minha_lista = [raquel_hp, raquel_attack, current_monster]
-
+#minha_lista = [raquel_hp, raquel_attack, current_monster]
+ 
 # Outro Ficheiro - Save Game (save.txt)
-with open("save.txt", "a") as f:
-  
-    for elemento in minha_lista:
 
-        f.write(f"{elemento}\n")
+def save(raquel):
+
+    with open("save.txt", "w") as f:
+
+        f.write(f"{raquel["hp"]}")
+        f.write(f"{raquel["attack"]}")
+        f.write(f"{raquel["current_monster"]}")
+    
+        print("Game Saved!")
+
+def load(raquel):
+
+    with open("save.txt") as f:
+
+        save_values = []
+
+        for line in f:
+            
+            value = line.strip()
+            save_values.append(value)
+
+        print(save_values)
+        raquel["hp"] = save_values[0]
+        raquel["attack"] = save_values[1]
+        raquel["current_monster"] = save_values[2]
+
+        print("Game Loaded!")
+
+
+load(raquel)
+
+
+
+while True:
+
+    print("Indica o que queres fazer")
+    print("a) balblabla")
+
+    opcao = input("Opção > ")
+
+    if opcao == "a":
+        ler()
+        break
+    elif opcao == "b":
+        escrever()
+        break
+    else:
+        print("Opção inválida")
+        break
+        
+
