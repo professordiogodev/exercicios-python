5c44d8973aab545641fcde30a4f26cfa73c56a13
raquel <ifkelly05@gmail.com>
2026-01-13 15:08:54 +0000

    resolução dos ex #13-01-2026

--- while.py
+++ while.py
@@ -12,10 +12,13 @@ experiencia_por_nivel = [50, 100, 200, 400, 800, 1600]
 #   subir_nivel(raquel)
 
 def subir_nivel(player):
-    # Restaurar Pontos de Vida
+    player["nivel"] += 1
+    player["xp"] = 0
+    player["max_hp"] += 5
     player["hp"] = player["max_hp"]
     player["max_attack"] += 1
     player["attack"] += 1
+    print(f"{player['name']} subiu para o nível {player['nivel']}!")
 
 # Função de Atacar
 def attack(source, target, is_critical = False):
@@ -55,9 +58,24 @@ def is_gameover(player1, player2):
 
 def raquel_attack(origin, target):
     attack(origin, target)
-    # Habilidade da Raquel - Curar-se 1 ponto de vida a cada ataque
-    print(f"{origin["name"]} curou-se 1 hp!")
     origin["hp"] += 1
+    if origin["hp"] > origin["max_hp"]:
+        origin["hp"] = origin["max_hp"]
+    print(f"{origin['name']} curou-se 1 HP")
+
+     # Habilidade da Raquel - Curar-se 1 ponto de vida a cada ataque
+
+def raquel_heal(origin):
+    origin["hp"] += 5
+    if origin["hp"] > origin["max_hp"]:
+        origin["hp"] = origin["max_hp"]
+    print(f"{origin['name']} curou-se 5 HP")
+
+def raquel_spell(origin, target):
+    print("Raquel lançou um Spell!")
+    target["hp"] -= 8
+    if target["hp"] < 0:
+        target["hp"] = 0
 
 raquel = {
     "name": "Raquel",
@@ -67,8 +85,11 @@ raquel = {
     "max_attack": 4,
     "gold": 0,
     "nivel": 1,
+    "xp": 0,
     "attack_logic": raquel_attack
 }
+
+raquel["xp"] = 0
 
 def slime_attack(origin, target):
     attack(origin, target)
@@ -78,6 +99,7 @@ slime = {
     "hp": 10,
     "attack": 2,
     "gold": 5,
+    "xp": 10,
     "attack_logic": slime_attack
 }
 
@@ -93,6 +115,7 @@ pixie = {
     "hp": 20,
     "attack": 3,
     "gold": 7,
+    "xp": 15,
     "attack_logic": pixie_attack
 }
 
@@ -115,7 +138,8 @@ goblin = {
     "name": "Goblin",
     "hp": 15,
     "attack": 3,
-    "gold": 10,
+    "gold": 20,
+    "xp": 20,
     "has_critical": False,
     "attack_logic": goblin_attack
 }
@@ -123,6 +147,8 @@ goblin = {
 def troll_attack(origin, target):
     attack(origin, target)
     target["attack"] -= 1
+    if target["attack"] < 1:
+        target["attack"] = 1
     print(f"{target["name"]} lost 1 Attack Point!")
 
 troll = {
@@ -130,10 +156,29 @@ troll = {
     "hp": 50,
     "attack": 6,
     "gold": 20,
+    "xp": 40,
     "attack_logic": troll_attack
 }
 
-monsters = [slime, pixie, goblin, troll]
+tomato = {
+    "name": "Tomato", 
+    "hp": 12, 
+    "attack": 1, 
+    "gold": 15, 
+    "xp": 25, 
+    "attack_logic": tomato_attack
+
+}
+
+def tomato_attack(origin, target):
+    if origin["hp"] < 5:
+        print("Tomato explodiu!")
+        origin["hp"] = 0
+        target["hp"] -= 20
+    else:
+        attack(origin, target)
+
+monsters = [slime, pixie, goblin, troll, tomato]
 
 for monster in monsters:
     while not is_gameover(raquel, monster):
@@ -151,15 +196,29 @@ for monster in monsters:
                 break
             elif opção == "b":
                 print("Raquel cura")
-                # raquel["heal"] <- Falta Implementar !!!!
+                raquel["heal"](raquel)
                 break
             elif opção == "c":
                 print("Raquel spell")
-                # raquel["spell"] <- Falta Implementar !!!!!!!!
+                raquel["spell"](raquel, monster)
                 break
-            
+
+
+    if raquel["hp"] > 0:
+        raquel["gold"] += monster["gold"]
+        raquel["xp"] += monster["xp"]
+        print(f"Raquel ganhou {monster['gold']} gold e {monster['xp']} XP")
+        check_level_up(raquel)
+
+        raquel["hp"] += 5
+        if raquel["hp"] > raquel["max_hp"]:
+            raquel["hp"] = raquel["max_hp"]
+        print("Raquel recuperou 5 HP após a luta")
 
         monster["attack_logic"](monster, raquel)
+        raquel["gold"] += monster["gold"]
+        print(f"Raquel ganhou {monster['gold']} gold! Total: {raquel['gold']}")
+
         print("-- Turn End -- \n")
 
     print("\n -- BATTLE END -- \n")
